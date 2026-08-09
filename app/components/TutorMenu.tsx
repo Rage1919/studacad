0b62878e849a402e178a2be9e7bbf230fdee0662
Rage1919 <315068125+Rage1919@users.noreply.github.com>
2026-08-09 22:56:12 +0200

    Preserve Pages base path for tutor links

--- app/components/TutorMenu.tsx
+++ app/components/TutorMenu.tsx
@@ -1,15 +1,23 @@
 "use client";
 
 import Link from "next/link";
+import type { MouseEvent } from "react";
+
+function followRenderedHref(event: MouseEvent<HTMLAnchorElement>) {
+  if (event.button !== 0 || event.metaKey || event.ctrlKey || event.shiftKey || event.altKey) return;
+
+  event.preventDefault();
+  window.location.assign(event.currentTarget.href);
+}
 
 export function TutorMenu() {
   return <div className="tutor-menu">
-    <Link href="/tutors" aria-haspopup="menu">Find tutors</Link>
+    <Link href="/tutors" aria-haspopup="menu" onClick={followRenderedHref}>Find tutors</Link>
     <div className="tutor-menu-panel" role="menu">
-      <Link role="menuitem" href="/favourites"><span>♥</span><b>Favourite tutors</b></Link>
-      <Link role="menuitem" href="/tutors?exam=PSLE"><span>PSLE</span><b>PSLE tutors</b></Link>
-      <Link role="menuitem" href="/tutors?exam=JCE"><span>JCE</span><b>JCE tutors</b></Link>
-      <Link role="menuitem" href="/tutors?exam=BGCSE"><span>BGCSE</span><b>BGCSE tutors</b></Link>
+      <Link role="menuitem" href="/favourites" onClick={followRenderedHref}><span>♥</span><b>Favourite tutors</b></Link>
+      <Link role="menuitem" href="/tutors?exam=PSLE" onClick={followRenderedHref}><span>PSLE</span><b>PSLE tutors</b></Link>
+      <Link role="menuitem" href="/tutors?exam=JCE" onClick={followRenderedHref}><span>JCE</span><b>JCE tutors</b></Link>
+      <Link role="menuitem" href="/tutors?exam=BGCSE" onClick={followRenderedHref}><span>BGCSE</span><b>BGCSE tutors</b></Link>
     </div>
   </div>;
 }
