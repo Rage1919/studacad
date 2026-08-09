f482fe069a3cc99823f9de3eb701d809f75feb87
Rage1919 <315068125+Rage1919@users.noreply.github.com>
2026-08-09 22:47:51 +0200

    Make Find tutors a direct link

--- app/components/TutorMenu.tsx
+++ app/components/TutorMenu.tsx
@@ -1,15 +1,11 @@
 "use client";
 
 import Link from "next/link";
-import { useState } from "react";
 
 export function TutorMenu() {
-  const [open, setOpen] = useState(false);
-
-  return <div className={`tutor-menu ${open ? "open" : ""}`} onMouseLeave={() => setOpen(false)}>
-    <button type="button" aria-haspopup="menu" aria-expanded={open} onClick={() => setOpen(value => !value)}>Find tutors</button>
+  return <div className="tutor-menu">
+    <Link href="/tutors" aria-haspopup="menu">Find tutors</Link>
     <div className="tutor-menu-panel" role="menu">
-      <Link role="menuitem" href="/tutors"><span>All</span><b>Browse every tutor</b></Link>
       <Link role="menuitem" href="/favourites"><span>♥</span><b>Favourite tutors</b></Link>
       <Link role="menuitem" href="/tutors?exam=PSLE"><span>PSLE</span><b>PSLE tutors</b></Link>
       <Link role="menuitem" href="/tutors?exam=JCE"><span>JCE</span><b>JCE tutors</b></Link>
